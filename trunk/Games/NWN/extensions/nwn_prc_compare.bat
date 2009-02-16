002d000c4f7955289efb8ca0049127b4d6ac97fc
dougcrews <dougcrews@eda789ac-d05c-11dd-bc61-0ff7aae23933>
2009-02-16 21:38:26 +0000

    PrC 3.3b

--- trunk/Games/NWN/extensions/nwn_prc_compare.bat
+++ trunk/Games/NWN/extensions/nwn_prc_compare.bat
@@ -1,9 +1,12 @@
 @echo off
-set PRC=%CD%
-if exist .\src\. goto prcVersionSet
-set PRC=%NWN_HOME%\extensions\PrC3.3
-:prcVersionSet
+set lastDir=%CD%
+if "%1" == "" goto compareAll
 
+:compareOne
+call :findIt %1
+goto end
+
+:compareAll
 set MINE=%NWN_HOME%\extensions\Custom\Override
 for /F "usebackq" %%f in (`dir /b /s %MINE%\*.2da`) do call :findIt %%f
 for /F "usebackq" %%f in (`dir /b /s %MINE%\*.nss`) do call :findIt %%f
@@ -14,15 +17,16 @@ for /F "usebackq" %%f in (`dir /b /s %MINE%\*.nss`) do call :findIt %%f
 goto end
 
 :findIt
-REM echo %0 %*
+echo %0 %*
 set filename=%~n1%~x1
-REM echo Finding %filename%
-for /F "usebackq" %%g in (`dir /b /s %PRC%\src\%filename%`) do call :diffIt %1 %%g 2>NUL
+echo Finding %filename%
+for /F "usebackq" %%g in (`dir /b /s %NWN_HOME_PRC%\src\%filename%`) do call :diffIt %1 %%g 2>NUL
 goto :EOF
 
 :diffIt
-fc %1 %2
+fc %1 %2 >NUL
 if ERRORLEVEL 1 tortoisemerge /mine:%1 /base:%2
 goto :EOF
 
 :end
+cd %lastDir%
